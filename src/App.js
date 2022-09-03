da331fd0e52ea498064e84f3e2f5e04a9cbc9cb9
kalluriyogeswararao <kalluriyogesh9@gmail.com>
2022-09-03 03:43:41 +0000

    incomplete

---
+++ src/App.js
@@ -0,0 +1,434 @@
+import {Component} from 'react'
+import './App.css'
+
+const tabsList = [
+  {tabId: 'FRUIT', displayText: 'Fruits'},
+  {tabId: 'ANIMAL', displayText: 'Animals'},
+  {tabId: 'PLACE', displayText: 'Places'},
+]
+const imagesList = [
+  {
+    id: 'b11ec8ce-35c9-4d67-a7f7-07516d0d8186',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/orange-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/orange-thumbnail-img.png',
+    category: 'FRUIT',
+  },
+  {
+    id: '04ac6b9f-b7e7-45f7-a8fc-fd48f3f72526',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/panda-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/panda-thumbnail-img.png',
+    category: 'ANIMAL',
+  },
+  {
+    id: 'a132f546-5b2b-4c0d-b9e4-e524bdf904cc',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/zebra-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/zebra-thumbnail-img.png',
+    category: 'ANIMAL',
+  },
+  {
+    id: 'd89386da-94db-4275-9cb5-249c6e071a19',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/paris-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/paris-thumbnail-img.png',
+    category: 'PLACE',
+  },
+  {
+    id: 'd810bbb0-1683-407a-8db6-898fe7b75782',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/giraffe-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/giraffe-thumbnail-img.png',
+    category: 'ANIMAL',
+  },
+  {
+    id: '176aab62-e86a-4ccd-8b89-5b83c3f02506',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/taj-mahal-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/taj-mahal-thumbnail-img.png',
+    category: 'PLACE',
+  },
+  {
+    id: '0e8daf1b-45b0-4eb0-9dde-383fede78a9b',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/monkey-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/monkey-thumbnail-img.png',
+    category: 'ANIMAL',
+  },
+  {
+    id: '1a38bf4a-659d-4470-956c-56c1bedd26ac',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/cheetah-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/cheetah-thumbnail-img.png',
+    category: 'ANIMAL',
+  },
+  {
+    id: '8f2ebd70-4fdd-47a0-b4f9-a6c654b519ab',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/ooti-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/ooti-thumbnail-img.png',
+    category: 'PLACE',
+  },
+  {
+    id: '7a72c38e-a83d-48eb-b9ce-ae3c0361cc49',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/pineapple-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/pineapple-thumbnail-img.png',
+    category: 'FRUIT',
+  },
+  {
+    id: '97a33ed5-98ed-4c95-a8f0-1595880b3b69',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/strawberry-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/strawberry-thumbnail-img.png',
+    category: 'FRUIT',
+  },
+  {
+    id: '07e20159-a950-4c22-9ca8-5ed71563ae24',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/maldives-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/maldives-thumbnail-img.png',
+    category: 'PLACE',
+  },
+  {
+    id: '43883239-8a28-47dc-9e93-43ef31654c17',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/emerald-lake-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/emerald-thumbnail-lake-img.png',
+    category: 'PLACE',
+  },
+  {
+    id: '49865ac4-b5e8-4d04-893b-d69ad6004da8',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/watermelon-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/watermelon-thumbnail-img.png',
+    category: 'FRUIT',
+  },
+  {
+    id: '649ab251-7fd6-4d65-aa0f-39020ce25932',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/elephant-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/elephant-thumbnail-img.png',
+    category: 'ANIMAL',
+  },
+  {
+    id: '1d0d1c41-e05e-4820-8614-34ee5ada20e0',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/jammu-hills-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/jammu-thumbnail-hills-img.png',
+    category: 'PLACE',
+  },
+  {
+    id: '88b4ab36-a0c1-4c56-9ce5-3b80dd8c7669',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/fierce-coyote-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/fierce-thumbnail-coyote-img.png',
+    category: 'ANIMAL',
+  },
+  {
+    id: '8a841bf8-3222-44da-b0fb-4c60190402d7',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/lidder-valley-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/lidder-thumbnail-valley-img.png',
+    category: 'PLACE',
+  },
+  {
+    id: 'd406e63c-eaaf-49ea-88a6-ed6a1572eb97',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/kivi-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/kivi-thumbnail-img.png',
+    category: 'FRUIT',
+  },
+  {
+    id: 'e997ebf9-9a47-4b7e-9035-01ae372d73dc',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/dragon-fruit-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/dragon-thumbnail-fruit-img.png',
+    category: 'FRUIT',
+  },
+  {
+    id: 'c7fbe10e-3282-4fca-815b-91b75d5228cb',
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/match-game/goa-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/goa-thumbnail-img.png',
+    category: 'PLACE',
+  },
+  {
+    id: '4210274c-7304-44d6-8690-c5251252cd10',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/papaya-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/papaya-thumbnail-img.png',
+    category: 'FRUIT',
+  },
+  {
+    id: '057b6193-a80d-4036-9e6e-fe847c99fbb6',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/mixed-fruits-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/mixed-thumbnail-fruits-img.png',
+    category: 'FRUIT',
+  },
+  {
+    id: '4e56c59b-835b-4802-87fe-77aaaa5b9526',
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/match-game/fox-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/fox-thumbnail-img.png',
+    category: 'ANIMAL',
+  },
+  {
+    id: 'ad75a7b1-0875-4700-977b-2c45924509aa',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/lotus-temple-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/lotus-thumbnail-temple-img.png',
+    category: 'PLACE',
+  },
+  {
+    id: '525aba17-ed5c-4f09-ad1c-b6bff222c97a',
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/match-game/dog-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/dog-thumbnail-img.png',
+    category: 'ANIMAL',
+  },
+  {
+    id: 'c6c66b00-c130-47d2-9d3a-1c3378d08aba',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/apple-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/apple-thumbnail-img.png',
+    category: 'FRUIT',
+  },
+  {
+    id: '6078b408-4f10-46d3-8815-db14403dbd73',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/bhadrinath-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/bhadrinath-thumbnail-img.png',
+    category: 'PLACE',
+  },
+  {
+    id: 'a2baca84-3beb-49d1-bced-f9a88c161bec',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/camel-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/camel-thumbnail-img.png',
+    category: 'ANIMAL',
+  },
+  {
+    id: '1edac278-8390-4da9-b914-5f41fb49283c',
+    imageUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/cherry-img.png',
+    thumbnailUrl:
+      'https://assets.ccbp.in/frontend/react-js/match-game/cherry-thumbnail-img.png',
+    category: 'FRUIT',
+  },
+]
+
+const TabItem = props => {
+  const {tabitem, clickTabItem, isActive} = props
+  const {displayText, tabId} = tabitem
+
+  const onClickTab = () => {
+    clickTabItem(tabId)
+  }
+
+  const activeBtn = isActive ? 'active-btn' : ''
+
+  return (
+    <li className="tab-item">
+      <button
+        type="button"
+        className={`button ${activeBtn}`}
+        onClick={onClickTab}
+      >
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+const ImageItem = props => {
+  const {imageitem, selectImage} = props
+  const {thumbnailUrl, id} = imageitem
+  const onClickImage = () => {
+    selectImage(id)
+  }
+
+  return (
+    <li className="each-image">
+      <button type="button">
+        <img
+          src={thumbnailUrl}
+          alt="thumbnail"
+          className="image"
+          onClick={onClickImage}
+        />
+      </button>
+    </li>
+  )
+}
+
+const countDown = 60
+
+class App extends Component {
+  state = {
+    activeTabId: tabsList[0].tabId,
+    randomImage: imagesList[0],
+    score: 0,
+    gameStatus: true,
+  }
+
+  navBar = () => {
+    const {score} = this.state
+    const GAME_LOGO =
+      'https://assets.ccbp.in/frontend/react-js/match-game-website-logo.png'
+
+    return (
+      <nav className="navbar">
+        <img src={GAME_LOGO} alt="website logo" className="web-logo" />
+
+        <div className="score-details">
+          <p className="score">
+            Score: <span className="span">{score}</span>
+          </p>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/match-game-timer-img.png"
+            alt="timer"
+            className="timer"
+          />
+          <p className="span">{countDown} Sec</p>
+        </div>
+      </nav>
+    )
+  }
+
+  clickTabItem = tabId => {
+    this.setState({activeTabId: tabId})
+  }
+
+  allTabsDisplay = () => {
+    const {activeTabId} = this.state
+    return (
+      <ul className="all-tabs">
+        {tabsList.map(each => (
+          <TabItem
+            tabitem={each}
+            key={each.tabId}
+            clickTabItem={this.clickTabItem}
+            isActive={each.tabId === activeTabId}
+          />
+        ))}
+      </ul>
+    )
+  }
+
+  selectImage = id => {
+    const {randomImage} = this.state
+    const index = Math.ceil(Math.random() * 29)
+
+    if (randomImage.id === id) {
+      const random = imagesList[index]
+      this.setState(prevState => ({
+        score: prevState.score + 1,
+        randomImage: random,
+      }))
+    } else {
+      this.setState({gameStatus: false})
+    }
+  }
+
+  allImagesDisplay = filterImages => {
+    const {activeTabId} = this.state
+    return (
+      <ul className="all-images">
+        {filterImages.map(eachImage => (
+          <ImageItem
+            imageitem={eachImage}
+            key={eachImage.id}
+            activeTab={activeTabId}
+            selectImage={this.selectImage}
+          />
+        ))}
+      </ul>
+    )
+  }
+
+  filteredImages = () => {
+    const {activeTabId} = this.state
+    const filterImages = imagesList.filter(
+      eachImageItem => eachImageItem.category === activeTabId,
+    )
+    return filterImages
+  }
+
+  displayRandomImage = () => {
+    const {randomImage} = this.state
+    const {imageUrl} = randomImage
+    return <img src={imageUrl} alt="match" className="random-image" />
+  }
+
+  playAgain = () => {
+    this.setState({gameStatus: true, score: 0})
+  }
+
+  gameOverPage = () => {
+    const {score} = this.state
+
+    return (
+      <div className="game-over">
+        <h1 className="score-data">Your Score</h1>
+        <p className="final-score">{score}</p>
+        <button
+          type="button"
+          className="play-again-btn "
+          onClick={this.playAgain}
+        >
+          PLAY AGAIN
+        </button>
+      </div>
+    )
+  }
+
+  render() {
+    const {gameStatus} = this.state
+    const filterImages = this.filteredImages()
+    return (
+      <div className="bg-container">
+        {this.navBar()}
+
+        {gameStatus && (
+          <div className="tab-details">
+            {this.displayRandomImage()}
+            {this.allTabsDisplay()}
+            {this.allImagesDisplay(filterImages)}
+          </div>
+        )}
+        <div className="game-page">{!gameStatus && this.gameOverPage()}</div>
+      </div>
+    )
+  }
+}
+
+export default App
